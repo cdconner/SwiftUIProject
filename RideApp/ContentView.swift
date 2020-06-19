7fd583a36f5fdef71f952ad44dcf4ba63ea8b326
cdconner <chris.conner@gmail.com>
2020-06-19 17:15:02 -0500

    Initial Commit

---
+++ RideApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RideApp
+//
+//  Created by Christopher Conner on 6/19/20.
+//  Copyright © 2020 Christopher Conner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
